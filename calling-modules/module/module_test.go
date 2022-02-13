5007a27ce23f13d95065c73424e32c0e8bfab1f3
ReyhanPatria <reyhan2patria@gmail.com>
2022-02-13 19:51:55 +0700

    Added test for GenerateHelloMessage success scenario

---
+++ calling-modules/module/module_test.go
@@ -0,0 +1,21 @@
+package module
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestGenerateHelloMessage_ShouldSucceed_WhenNameIsNotBlank(t *testing.T) {
+	// given
+	name := "Reyhan"
+
+	// when
+	message, err := GenerateHelloMessage(name)
+
+	// then
+	expected := regexp.MustCompile(`\b` + name + `\b`)
+
+	if !expected.MatchString(message) || err != nil {
+		t.Fatalf("GenerateHelloMessage(%s) failed with error '%s'", name, err)
+	}
+}
