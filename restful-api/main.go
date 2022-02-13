c83119b25001091606eb02e77a959035324e6a72
ReyhanPatria <reyhan2patria@gmail.com>
2022-02-13 23:24:45 +0700

    Added handler function for getting all products

--- restful-api/main.go
+++ restful-api/main.go
@@ -1,11 +1,20 @@
 package main
 
-import "reyhan/restful-api/model"
+import (
+	"net/http"
+	"reyhan/restful-api/model"
+
+	"github.com/gin-gonic/gin"
+)
 
 var products = []model.Product{
 	{ID: 1, Name: "Apple", Price: 23000, Description: "Apple fresh from Aomori Prefecture"},
 	{ID: 2, Name: "Banana", Price: 25000, Description: "Banana from the Banana Republic"},
 	{ID: 3, Name: "Steam", Price: 0, Description: "It's the software, not actual steam"},
+}
+
+func getProducts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, products)
 }
 
 func main() {
