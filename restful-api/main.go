b08012c91c88fdf0cd90569fb54879936ebc8a37
ReyhanPatria <reyhan2patria@gmail.com>
2022-02-13 23:24:19 +0700

    Added products data

--- restful-api/main.go
+++ restful-api/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "reyhan/restful-api/model"
 
+var products = []model.Product{
+	{ID: 1, Name: "Apple", Price: 23000, Description: "Apple fresh from Aomori Prefecture"},
+	{ID: 2, Name: "Banana", Price: 25000, Description: "Banana from the Banana Republic"},
+	{ID: 3, Name: "Steam", Price: 0, Description: "It's the software, not actual steam"},
+}
+
+func main() {
 }
