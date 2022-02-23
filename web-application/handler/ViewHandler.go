d6bc99fb3bc860cd971b448bd7af58e24841bc41
ReyhanPatria <reyhan2patria@gmail.com>
2022-02-23 20:05:35 +0700

    Added explantion comment to funciton handlers

--- web-application/handler/ViewHandler.go
+++ web-application/handler/ViewHandler.go
@@ -6,6 +6,7 @@ import (
 	"reyhan/web-application/model"
 )
 
+// Index function handler
 func GetIndexView(w http.ResponseWriter, r *http.Request) {
 	// Handle if http method is not GET
 	if r.Method != http.MethodGet {
@@ -26,6 +27,7 @@ func GetIndexView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", page.Title, page.Body)
 }
 
+// Wiki view function handler
 func GetWikiView(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusMethodNotAllowed)
