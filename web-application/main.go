b29280ca4f88cc02fdb96a78627b939d9c3d8258
ReyhanPatria <reyhan2patria@gmail.com>
2022-02-25 23:41:32 +0700

    Changed theme from wiki to blog

--- web-application/main.go
+++ web-application/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler.GetIndexView)
-	http.HandleFunc("/view/", handler.GetWikiView)
+	http.HandleFunc("/view/", handler.GetBlogView)
 
 	http.ListenAndServe("localhost:8080", nil)
 }
